8b17521b2be60e32e6c102e8fcbd5097d0b6c760
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-28 18:45:43 +0700

    Menginstall Dependency yang kurang sama Setup file server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Koneksi ke MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected on mongodb://localhost:27017/task-management"))
+  .catch((err) => console.error("Error: ", err));
+
+// Jalankan server
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
