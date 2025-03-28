8e19a9330b5bf480874691908c20648395b3250a
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-28 22:35:40 +0700

    Bikin Task models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+// const Task = require ('./models/Task')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
