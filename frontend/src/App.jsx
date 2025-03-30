b2da97b1d78429de412fa4f6aa20cc41d987111b
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-31 01:41:01 +0700

    Bikin Struktur Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Task Management</h1>
+      <p>Selamat datang di aplikasi manajemen tugas!</p>
+    </div>
+  );
+}
+export default App;
