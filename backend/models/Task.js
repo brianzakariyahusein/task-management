3de31ee115d3bddba11c0745bb8918a6f7fbf697
Brian Zakariya <zakariyabrian@gmail.com>
2025-04-09 22:20:20 +0700

    Membuat Task Model untuk MongoDB

---
+++ backend/models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// Definisi Schema untuk Task
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "in-progress", "completed"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Membuat model berdasarkan schema yang sudah dibuat
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
