2f4fc0a265674ee272b1fe693161f02ae3cdcd75
Brian Zakariya <zakariyabrian@gmail.com>
2025-04-10 23:34:24 +0700

    Membuat Task Model

---
+++ backend/models/Task.js
@@ -0,0 +1,30 @@
+// Mengimport mongoose
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low",
+    },
+    dueDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true } // Menambahkan timestamps untuk createdAt dan updatedAt
+);
+
+module.exports = mongoose.model("Task", taskSchema);
