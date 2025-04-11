72561405565cfed73ec67cf6d185c9c3f110b016
Brian Zakariya <zakariyabrian@gmail.com>
2025-04-12 00:26:39 +0700

    Membuat Rute / Mengaktifkan Rute di Route

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,15 +1,21 @@
 // Import Express and Task Controller
 const express = require("express");
-const { getAllTasks, createTask } = require("../controllers/taskController");
+const {
+  getAllTasks,
+  createTask,
+  getTaskById,
+  updateTaskById,
+  deleteTaskById,
+} = require("../controllers/taskController");
 
 // Create a new router object
 const router = express.Router();
 
 router.get("/", getAllTasks); // Get all tasks
 router.post("/", createTask); // Create a new task
-// router.get('/:id', getTaskById); // Get task by ID
-// router.put('/:id', updateTaskById); // Update task by ID
-// router.delete('/:id', deleteTaskById); // Delete task by ID
+router.get("/:id", getTaskById); // Get task by ID
+router.put("/:id", updateTaskById); // Update task by ID
+router.delete("/:id", deleteTaskById); // Delete task by ID
 
 // Export the router
 module.exports = router;
